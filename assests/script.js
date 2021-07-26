733d441c9b3ecea6eff139bb225b8d7de6e8d0f8
Micheal Limones <Micheal.Limones@du.edu>
2021-07-25 21:17:56 -0600

    added function to add items to local storage. added function to retrieve items from local storage.

--- assests/script.js
+++ assests/script.js
@@ -1,15 +1,51 @@
+//I'm not uses these two variables 
 var hourEl = $('.hour');
 var descriptionEl = $(".description");
-var saveBtnEl = $(".saveBtn");
-var timeBlockEl = $(".time-block")
 
-//will remove this later. just adding so I can see time on page
-var timeEl = $('#time');
+
+// I am using this one
+var saveBtnEl = $(".saveBtn");
+
 
 var today = moment();
 $("#currentDay").text(today.format("MMM Do, YYYY"));
 
-rightNow = moment().hour();
+var rightNow = moment().hour();
+
+
+var businessHour = [$("#8"), $("#9"), $("#10"), $("#11"), $("#12"), $("#13"), $("#14"), $("#15"), $("#16")];
+
+
+businessHour.forEach(element => {
+    var previousInput = localStorage.getItem(element.attr("id"))
+    element.siblings('.description').val(previousInput)
+})
+
+
+
+
+businessHour.forEach(element => {
+    if (element.attr("id") < rightNow) {
+     element.parent().addClass('past')
+    }
+     else if (element.attr("id") == rightNow) {
+        element.parent().addClass('present')
+    } else {
+        element.parent().addClass('future')
+    }
+});
+
+
+saveBtnEl.click(function () {
+    var userInput = $(this).siblings(".description").val();
+    var timeSlot = $(this).siblings(".hour").attr('id');
+
+    localStorage.setItem(timeSlot, userInput)
+})
+
+
+// var rightNow = 12;
+
 
 // function currentTime() {
 //     var rightNow = moment().hour();
@@ -19,48 +55,21 @@ rightNow = moment().hour();
 
 // setInterval(currentTime, 1000);
 
-// color coded to indicate whether it is in the past, present, or future
-// be able to enter an event when clicking on timeblock 
-// save button to save description to local Storage
+// console.log(businessHour[0].attr('id'))
 
-function timeOfDay() {
+// for( let i = 0; i < businessHour.length; i++){
+//      const element = businessHour[i].attr('id');
+//      console.log(element);
+//      if (element < rightNow ){
+//          console.log("this is before now")
+//          businessHour[i].parent().addClass('past')
 
-}
-
-var businessHour = [$("#8"), $("#9"), $("#10"), $("#11"), $("#12"), $("#13"), $("#14"), $("#15"), $("#16")];
-
-console.log(businessHour[0].attr('id'))
-
-for( let i = 0; i < businessHour.length; i++){
-     const element = businessHour[i].attr('id');
-     console.log(element);
-     if (element < rightNow ){
-         console.log("this is before now")
-         businessHour[i].parent().addClass('past')
-
-     } else if (element == rightNow) {
-         console.log("this is now")
-         businessHour[i].parent().addClass('present')
+//      } else if (element == rightNow) {
+//          console.log("this is now")
+//          businessHour[i].parent().addClass('present')
  
-     } else {
-         console.log("future")
-         businessHour[i].parent().addClass('future')
-     }
-}
-
-// businessHour.forEach(element => {
-//     if (element.attr("id") < rightNow) {
-//      console.log("this is before now")
-//      $('this').timeBlockEl.addClass('past')
-//     }
-//      else if (element.attr("id") == rightNow) {
-//         console.log("this is now")
-//         $('this').timeBlockEl.addClass('present')
-//     } else {
-//         console.log("future")
-//         $('this').timeBlockEl.addClass('future')
-//     }
-// });
-
-
-
+//      } else {
+//          console.log("future")
+//          businessHour[i].parent().addClass('future')
+//      }
+// }
